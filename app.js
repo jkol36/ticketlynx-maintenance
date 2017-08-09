66a44b78ef0314b1169c089a2217493216d2c12b
Jonathan Kolman <jonathankolman@Jonathans-MacBook-Pro.local>
2017-08-08 22:35:07 -0400

    remove user from firebase when deleted from webapp

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-import { onSaleRef } from './config'
+import { onSaleRef, userRef } from './config'
 import moment from 'moment'
+import firebaseAdmin from 'firebase-admin'
 
 
-const start = () => {
+const removeOnSaleRef = () => {
   return onSaleRef
   .remove()
   .then(() => console.log('done'))
 }
 
-setInterval(() => start(), 86100000)
+const listenForUserDeleted = () => {
+  userRef.on('child_removed', s => {
+    console.log('user deleted', s.val())
+    let {uid} = s.val()
+    return firebaseAdmin.auth().deleteUser(uid)
+  })
+}
+
+setInterval(() => removeOnSaleRef(), 86100000)
+listenForUserDeleted()
--- config.js
+++ config.js
@@ -12,3 +12,4 @@ export const firebase = firebaseAdmin.initializeApp({
 export const onSaleRef = firebase.database().ref('onSale')
 //create a firebase ref that serves as a signaling tool for the scrapers to run
 export const signalingRef = firebase.database().ref('signalingRef')
+export const userRef = firebase.database().ref('users')
